7956938b0164516c961dfb042985664bd1b1126e
Robert Gregg <hi@robertgregg.co.uk>
2021-11-21 15:50:45 +0000

    Finish basic styling

--- Components/ContactForm.js
+++ Components/ContactForm.js
@@ -13,39 +13,16 @@ const ContactForm = () => {
   }
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      <label htmlFor="name" className="py-2 text-grey-darkest">
-        Full Name
-      </label>
-      <input
-        className="border py-2 px-3 text-grey-darkest"
-        id="name"
-        type="name"
-        name="name"
-      />
+      <input id="name" type="name" name="name" placeholder="name" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <label htmlFor="email" className="py-2 text-grey-darkest">
-        Email Address
-      </label>
-      <input
-        className="border py-2 px-3 text-grey-darkest"
-        id="email"
-        type="email"
-        name="email"
-      />
+      <input id="email" type="email" name="email" placeholder="email" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <label className="py-2 text-grey-darkest" htmlFor="message">
         How can we help?
       </label>
-      <textarea
-        className="border py-2 px-3 text-grey-darkest"
-        id="message"
-        name="message"
-      />
+      <textarea id="message" name="message" placeholder="How can we help?" />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button
-        type="submit"
-        disabled={state.submitting}
-        className="bg-amazon_blue-light hover:bg-blue-900 text-white font-bold my-6 py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      <button type="submit" disabled={state.submitting}>
         Send
       </button>
     </form>
